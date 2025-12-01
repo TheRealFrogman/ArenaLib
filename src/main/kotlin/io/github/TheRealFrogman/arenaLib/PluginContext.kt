94cb1cf2d840ca5de2cf4fd0deca96348ce75480
frogman <74905689+TheRealFrogman@users.noreply.github.com>
2025-12-01 18:46:36 +0500

    удалил пробел

--- src/main/kotlin/io/github/TheRealFrogman/arenaLib/PluginContext.kt
+++ src/main/kotlin/io/github/TheRealFrogman/arenaLib/PluginContext.kt
@@ -3,6 +3,5 @@ package io.github.TheRealFrogman.arenaLib
 import io.github.TheRealFrogman.arenaLib.Core.ArenaPlayer.ArenaPlayerRepository
 
 class PluginContext {
-
     val arenaPlayerRepository = ArenaPlayerRepository()
 }
