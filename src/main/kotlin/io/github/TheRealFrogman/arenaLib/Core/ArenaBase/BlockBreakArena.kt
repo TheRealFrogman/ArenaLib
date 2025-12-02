d44cc0d5934dab5d0a694f3f51205cfe11a55345
frogman <74905689+TheRealFrogman@users.noreply.github.com>
2025-12-02 17:34:01 +0500

    добавил новый тип арены

---
+++ src/main/kotlin/io/github/TheRealFrogman/arenaLib/Core/ArenaBase/BlockBreakArena.kt
@@ -0,0 +1,64 @@
+package io.github.TheRealFrogman.arenaLib.Core.ArenaBase
+
+import io.github.TheRealFrogman.arenaLib.ArenaLib
+import io.github.TheRealFrogman.arenaLib.Core.ArenaPlayer.ArenaPlayer
+import io.github.TheRealFrogman.arenaLib.Core.Components.Mandatory.ArenaRegion.ArenaRegion
+import io.github.TheRealFrogman.arenaLib.Core.Components.Mandatory.SpawnPoint.SpawnPoint
+import io.github.TheRealFrogman.arenaLib.Core.Components.Mandatory.Team.Team
+import org.bukkit.block.Block
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.block.BlockBreakEvent
+import org.bukkit.plugin.java.JavaPlugin
+
+abstract class BlockBreakArena(
+    name: String,
+    region: ArenaRegion,
+    spawnPoints: MutableList<SpawnPoint>,
+    teamsInitializers: MutableList<Team.Initializer>,
+    plugin: JavaPlugin,
+) : ArenaBase(name, region, spawnPoints, teamsInitializers, plugin) {
+
+    val pluginContext = ArenaLib.context
+
+    val eventHook = object : Listener {
+
+        @EventHandler
+        private fun onBlockBreak(event: BlockBreakEvent) {
+            val maybeArenaPlayer = pluginContext.arenaPlayerRepository.getArenaPlayer(event.player.uniqueId)
+            if (maybeArenaPlayer == null)
+                return
+
+            val arenaPlayer = maybeArenaPlayer as ArenaPlayer
+
+            val isfRegionContainsBlock = region.contains(event.block.location)
+
+            val valid = isPlayerInArena(arenaPlayer) && isfRegionContainsBlock
+
+            if (valid) {
+                brokenBlocks.computeIfAbsent(arenaPlayer) { mutableListOf() }.add(event.block)
+                onBukkitBlockBreak(arenaPlayer, event.block)
+            }
+        }
+    }
+
+    init {
+        plugin.getServer()
+            .getPluginManager()
+            .registerEvents(this.eventHook, plugin)
+    }
+
+    val brokenBlocks: MutableMap<ArenaPlayer, MutableList<Block>> = HashMap()
+
+    // возвращаем блоки
+    private fun restoreArena() {
+        TODO("Not yet implemented")
+        TODO("восстановить арену асинхронно")
+    }
+
+    override fun onFinish() {
+        TODO("Not yet implemented")
+        restoreArena()
+    }
+    protected abstract fun onBukkitBlockBreak(arenaPlayer: ArenaPlayer, block: Block)
+}
