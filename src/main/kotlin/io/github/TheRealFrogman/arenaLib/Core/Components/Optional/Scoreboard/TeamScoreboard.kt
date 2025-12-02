a2203d410e5619bb06eea89eb93d9f104d0e259a
frogman <74905689+TheRealFrogman@users.noreply.github.com>
2025-12-02 17:28:40 +0500

    implement team scoreboard

--- src/main/kotlin/io/github/TheRealFrogman/arenaLib/Core/Components/Optional/Scoreboard/TeamScoreboard.kt
+++ src/main/kotlin/io/github/TheRealFrogman/arenaLib/Core/Components/Optional/Scoreboard/TeamScoreboard.kt
@@ -3,13 +3,56 @@ package io.github.TheRealFrogman.arenaLib.Core.Components.Optional.Scoreboard
 import io.github.TheRealFrogman.arenaLib.Core.ArenaBase.ArenaBase
 import io.github.TheRealFrogman.arenaLib.Core.Components.ArenaComponent
 import io.github.TheRealFrogman.arenaLib.Core.Components.Mandatory.Team.Team
+import java.util.Collections
+import java.util.HashMap
+import java.util.Map
 
 class TeamScoreboard(
     arena: ArenaBase,
     private val onScoreChanged: OnScoreChanged
-) : ArenaComponent(arena) {
+) : ArenaComponent(arena), IScoreboard<Team> {
 
     fun interface OnScoreChanged {
-        fun execute(team: Team, score: Int)
+        fun execute(team: Team, score: Long)
+    }
+
+    private val scoreboard: MutableMap<Team, Long> = HashMap()
+
+    override fun addScore(team: Team, amount: Long) {
+        scoreboard.put(team, scoreboard.get(team)!! + amount)
+        onScoreChanged.execute(team, scoreboard.get(team)!!)
+    }
+
+    override fun setScore(team: Team, score: Long) {
+        scoreboard.put(team, score)
+        onScoreChanged.execute(team, score)
+    }
+
+    override fun getScore(team: Team): Long = this.scoreboard.get(team)!!
+
+    override val leader: Team?
+        get() = Collections.max<MutableMap.MutableEntry<Team, Long>?>(
+            scoreboard.entries,
+            Map.Entry.comparingByValue<Team, Long>()
+        )?.key
+
+    override fun getFirstLeading(amount: Long): List<Team> {
+        val result = ArrayList<Team>()
+
+        val it = scoreboard.entries.iterator()
+
+        while (it.hasNext() && result.size < amount) {
+            val entry = it.next()
+            result.add(entry.key)
+        }
+
+        return result
+    }
+
+    override fun getAllSortedByScore(): MutableMap<Team, Long> {
+        return scoreboard.entries
+            .sortedByDescending { it.value }
+            .associate { it.key to it.value }
+            .toMutableMap()
     }
 }
